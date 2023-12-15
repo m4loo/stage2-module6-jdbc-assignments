185b1048e76cf1468f8dc7a852c899ee81164501
m4loo <128167781+m4loo@users.noreply.github.com>
2023-12-15 20:41:48 +0500

    done?

--- src/main/java/jdbc/CustomConnector.java
+++ src/main/java/jdbc/CustomConnector.java
@@ -1,35 +1,24 @@
 package jdbc;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class CustomConnector {
     public Connection getConnection(String url) {
-        Connection connection = null;
-
         try {
-            connection = DriverManager.getConnection(url);
-        }catch (Exception e){
-            e.printStackTrace();
+            return DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-
-        return connection;
     }
 
-    public Connection getConnection(String url, String user, String password){
-        Connection connection = null;
-
+    public Connection getConnection(String url, String user, String password) {
         try {
-            connection = DriverManager.getConnection(url, user, password);
-        }catch (Exception e){
-            e.printStackTrace();
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
 
-        return connection;
     }
 }
--- src/main/java/jdbc/CustomDataSource.java
+++ src/main/java/jdbc/CustomDataSource.java
@@ -1,31 +1,28 @@
 package jdbc;
 
-import javax.sql.DataSource;
-
 import lombok.Getter;
 import lombok.Setter;
-import lombok.Value;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.SQLFeatureNotSupportedException;
-import java.util.Objects;
 import java.util.Properties;
 import java.util.logging.Logger;
 
 @Getter
 @Setter
 public class CustomDataSource implements DataSource {
+    private static final SQLException SQL_EXCEPTION = new SQLException();
+    private static final Object MONITOR = new Object();
     private static volatile CustomDataSource instance;
     private final String driver;
     private final String url;
     private final String name;
     private final String password;
+    private Connection connection;
 
     private CustomDataSource(String driver, String url, String password, String name) {
         this.driver = driver;
@@ -33,72 +30,71 @@ public class CustomDataSource implements DataSource {
         this.name = name;
         this.password = password;
     }
-
     public static CustomDataSource getInstance() {
-        if (instance != null) return instance;
-
-        try {
-            Properties appProps = new Properties();
-            appProps.load(
-                    CustomDataSource.class.getClassLoader().getResourceAsStream("app.properties")
-            );
-
-            instance = new CustomDataSource(
-                    appProps.getProperty("postgres.driver"),
-                    appProps.getProperty("postgres.url"),
-                    appProps.getProperty("postgres.password"),
-                    appProps.getProperty("postgres.name")
-            );
-        } catch (IOException e){
-            e.printStackTrace();
+        if (instance == null) {
+            synchronized (CustomDataSource.class) {
+                if (instance == null) {
+                    try {
+                        Properties properties = new Properties();
+                        properties.load(CustomDataSource.class.getClassLoader().getResourceAsStream("app.properties"));
+                        instance = new CustomDataSource(
+                                properties.getProperty("postgres.driver"),
+                                properties.getProperty("postgres.url"),
+                                properties.getProperty("postgres.password"),
+                                properties.getProperty("postgres.name")
+                        );
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
         }
 
         return instance;
     }
 
-
     @Override
-    public Connection getConnection(){
+    public Connection getConnection() {
         return new CustomConnector().getConnection(url, name, password);
     }
 
     @Override
     public Connection getConnection(String username, String password) {
-        return new CustomConnector().getConnection(url, name, password);
+        return new CustomConnector().getConnection(url, username, password);
     }
 
     @Override
     public PrintWriter getLogWriter() throws SQLException {
-        return null;
+        throw SQL_EXCEPTION;
     }
 
     @Override
     public void setLogWriter(PrintWriter out) throws SQLException {
-
-    }
-
-    @Override
-    public void setLoginTimeout(int seconds) throws SQLException {
-
+        throw SQL_EXCEPTION;
     }
 
     @Override
     public int getLoginTimeout() throws SQLException {
-        return 0;
+        throw SQL_EXCEPTION;
+    }
+
+    @Override
+    public void setLoginTimeout(int seconds) throws SQLException {
+        throw SQL_EXCEPTION;
     }
 
     @Override
     public Logger getParentLogger() throws SQLFeatureNotSupportedException {
-        return null;
+        throw new SQLFeatureNotSupportedException();
     }
 
     @Override
     public <T> T unwrap(Class<T> iface) throws SQLException {
-        return null;
+        throw SQL_EXCEPTION;
     }
 
     @Override
     public boolean isWrapperFor(Class<?> iface) throws SQLException {
-        return false;
+        throw SQL_EXCEPTION;
     }
 }
